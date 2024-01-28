a7f77680545e017512266e30ba0143688502599f
yw0822 <12624341+yw0822@users.noreply.github.com>
2024-01-28 22:32:08 +0800

    ✨ feat: x86 with 6.x kernel

--- scripts/diy-part2.sh
+++ scripts/diy-part2.sh
@@ -10,9 +10,9 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 
 # Modify default IP
-sed -i 's/192.168.1.1/192.168.66.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.100.1/g' package/base-files/files/bin/config_generate
 
 #修正连接数（by ベ七秒鱼ベ）
 sed -i '/customized in this file/a net.netfilter.nf_conntrack_max=165535' package/base-files/files/etc/sysctl.conf
 # 修改x86编译内核版本为5.15
-sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
+# sed -i 's/KERNEL_PATCHVER:=6.1/KERNEL_PATCHVER:=5.15/g' ./target/linux/x86/Makefile
