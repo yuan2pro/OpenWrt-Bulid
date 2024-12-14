40e722e41be9c97bfa3864c3b4a619bed4badd89
yuan2pro <ypjalt@gmail.com>
2024-12-14 23:26:09 +0800

    ✨ feat:update

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -12,6 +12,12 @@
 
 # https://github.com/kenzok8/small
 sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git third_party https://github.com/linkease/istore-packages' feeds.conf.default
+sed -i '$a src-git diskman https://github.com/jjm2473/luci-app-diskman.git' feeds.conf.default
+sed -i '$a src-git oaf https://github.com/jjm2473/OpenAppFilter.git' feeds.conf.default
+sed -i '$a src-git linkease_nas https://github.com/linkease/nas-packages.git' feeds.conf.default
+sed -i '$a src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci' feeds.conf.default
+sed -i '$a src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
