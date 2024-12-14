414474ae8db3bbfdbcf0fe30bb93cad2624007bc
yuan2pro <ypjalt@gmail.com>
2024-12-14 22:56:40 +0800

    ✨ feat: update

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -11,8 +11,7 @@
 
 
 # https://github.com/kenzok8/small
-sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
-sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
+sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
 ./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
 rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
 rm -rf feeds/packages/utils/v2dat
