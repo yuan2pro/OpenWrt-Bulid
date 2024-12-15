31d6a2cf17f4dac6e2a905a115be28deaaed2f41
yuan2pro <ypjalt@gmail.com>
2024-12-15 23:19:08 +0800

    🐞 fix: fix

--- scripts/diy-part3.sh
+++ scripts/diy-part3.sh
@@ -11,13 +11,17 @@
 
 
 # https://github.com/kenzok8/small
-sed -i '$a src-git smpackage https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
 sed -i '$a src-git third_party https://github.com/linkease/istore-packages' feeds.conf.default
 sed -i '$a src-git diskman https://github.com/jjm2473/luci-app-diskman.git' feeds.conf.default
 sed -i '$a src-git oaf https://github.com/jjm2473/OpenAppFilter.git' feeds.conf.default
 sed -i '$a src-git linkease_nas https://github.com/linkease/nas-packages.git' feeds.conf.default
 sed -i '$a src-git linkease_nas_luci https://github.com/linkease/nas-packages-luci' feeds.conf.default
 sed -i '$a src-git jjm2473_apps https://github.com/jjm2473/openwrt-apps.git' feeds.conf.default
-./scripts/feeds update -a
+./scripts/feeds update -a && rm -rf feeds/luci/applications/luci-app-mosdns
+rm -rf feeds/packages/net/{alist,adguardhome,mosdns,xray*,v2ray*,v2ray*,sing*,smartdns}
+rm -rf feeds/packages/utils/v2dat
 rm -rf feeds/packages/lang/golang
 git clone https://github.com/kenzok8/golang feeds/packages/lang/golang
+./scripts/feeds install -a 
