acbc7b7f5f2cfa31e9dfaf929a2ea6de3c53b891
Sheldon <ypjalt@gmail.com>
2024-12-17 13:57:38 +0800

    Update diy-part1.sh

--- scripts/diy-part1.sh
+++ scripts/diy-part1.sh
@@ -18,7 +18,7 @@
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 # echo 'src-git small8 https://github.com/kenzok8/small-package' >>feeds.conf.default
 # echo 'src-git yw0822 https://github.com/yw0822/openwrt-packages' >>feeds.conf.default
-echo 'src-git linkease https://github.com/linkease/istore.git' >>feeds.conf.default
+#echo 'src-git linkease https://github.com/linkease/istore.git' >>feeds.conf.default
 
 # git clone https://github.com/kenzok8/small-package package/small-package
 # git clone https://github.com/kiddin9/openwrt-packages package/openwrt-package
